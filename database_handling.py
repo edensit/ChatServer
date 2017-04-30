2ea2aa12ad1749d6e09e338d6521d49d4c8fd20e
edensit <eden.sit@gmail.com>
2017-04-30 14:31:19 +0300

    change logo file name

--- database_handling.py
+++ database_handling.py
@@ -1,4 +1,5 @@
 import sqlite3
+import hash_handling
 
 
 class BaseError(Exception):
@@ -15,13 +16,24 @@ class RegisterError(BaseError):
 
 class DatabaseHandler:
     def __init__(self):
-        self.conn = sqlite3.connect("usersdatabase.db")
-        self.sql_db = self.conn.cursor()
+        try:
+            self.conn = sqlite3.connect("usersdatabase.db")
+        except sqlite3.Error:
+            pass
+        else:
+            self.sql_db = self.conn.cursor()
 
     def is_correct_login(self, username, password):
-        self.sql_db.execute("SELECT * FROM users_table WHERE Username = ?"
-                            " COLLATE NOCASE AND Password = ?", (username, password))
-        return self.sql_db.fetchall()
+        self.sql_db.execute("SELECT * FROM users_table WHERE Username = ? COLLATE NOCASE", (username,))
+        auth_details = self.sql_db.fetchall()
+
+        hashed_password = auth_details[0][1]
+        salt = auth_details[0][4]
+        hashed_enterd_pass_salt = hash_handling.HashHandler.generate_hash(password, salt)
+
+        if hashed_password == hashed_enterd_pass_salt:
+            return True
+        return False
 
     def insert_new_user(self, username, password):
         try:
@@ -35,6 +47,7 @@ class DatabaseHandler:
 
 
 # http://stackoverflow.com/questions/1054022/best-way-to-store-password-in-database - HASHING & SALT
+# https://www.codeproject.com/Articles/704865/Salted-Password-Hashing-Doing-it-Right - HASHING
 # http://stackoverflow.com/questions/420843/how-does-password-salt-help-against-a-rainbow-table-attack?noredirect=1&lq=1
 
 
